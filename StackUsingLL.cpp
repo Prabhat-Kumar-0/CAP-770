4286fe5108f68b6df53be9bc3a4b20911f6e8bd5
Prabhat Kumar <115002585+Prabhat-Kumar-0@users.noreply.github.com>
2024-04-01 10:42:28 +0530

    Add files via upload

---
+++ StackUsingLL.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class stack{
+    int data;
+    stack* next;
+    public:
+    stack(int data){
+        this -> data = data;
+        this -> next = NULL; 
+    }
+}
+
+void insert(stack* top){
+
+}
+
+int main(){
+    stack *top;
+    
+}
